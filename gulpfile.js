d65fdc4ea94d33e68484cb7ae42348d081cf3587
do0g <do0g@users.noreply.github.com>
2015-10-08 11:52:40 +0100

    Mocha, Plumber and auto-load plugins.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,14 @@
-var gulp = require('gulp')
-    $    = require('gulp-load-plugins');
+var gulp     = require('gulp')
+    $        = require('gulp-load-plugins')();
+    manifest = require('./package.json'),
+    config   = manifest.config;
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('test', function() {
+  return gulp.src(['test/mocha_init.js', 'test/unit/**/*.js'], {read: false})
+             .pipe($.plumber())
+             .pipe($.mocha({reporter: 'dot', globals: config.mocha.globals }));
 });
